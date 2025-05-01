3121b1f239de188d2c2d843e1ba2dd0c07db2810
root <root@Ansible.desblock.net>
2025-05-01 21:13:44 +0000

    Modify with proper home variables and minor changes to use apt pipx and install on target hosts (assuming not installing locally)

--- setup.sh
+++ setup.sh
@@ -4,17 +4,18 @@ set -e
 sudo apt-get update -y
 
 # Install Python3
-sudo apt-get install -y python3 python3-dev python3-venv 
+sudo apt-get install -y python3 python3-dev python3-venv pipx
 
 # Install Pipx
-sudo apt-get purge -y pipx # Uninstall old pipx versions
-python3 -m pip install --user pipx --break-system-packages || python3 -m pip install pipx
+# sudo apt-get purge -y pipx # Uninstall old pipx versions
+# python3 -m pip install --user pipx --break-system-packages || python3 -m pip install pipx
 
 # Source .bashrc so pipx path will be found.
 export PATH="$PATH:$HOME/.local/bin"
 
 # Global ensurepath
-python3 -m pipx ensurepath --force
+# python3 -m pipx ensurepath --force
+sudo pipx ensurepath --global
 
 # Install Ansible
 pipx install --include-deps ansible
